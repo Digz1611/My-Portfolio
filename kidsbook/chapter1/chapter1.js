8d797576cf9e3b1af94c6fbb2779cafdebd966e8
Digz1611 <diego.langeveldt@gmail.com>
2024-03-19 14:41:09 +0200

    sorted file, just re-reference

--- kidsbook/chapter1/chapter1.js
+++
@@ -1,9 +0,0 @@
-/*
-Draw as many cats as you want!
-*/
-var drawCats = function (howManyTimes) {
-    for (var i = 0; i < howManyTimes; i++) {
-        console.log(i + " =^.^=");
-    }
-};
-drawCats(20); // You can put any number here instead of 10.  
